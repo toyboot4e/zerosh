8d76069c487de3e6f716564a0b9a7a80002861e5
toyboot4e <toyboot4e@gmail.com>
2022-12-17 18:03:56 +0900

    wip main loop

--- src/lib.rs
+++ src/lib.rs
@@ -56,28 +56,24 @@ pub fn run_shell(sh: &Shell) -> Result<(), DynError> {
         signal::signal(signal::Signal::SIGTTOU, signal::SigHandler::SigIgn).unwrap();
     }
 
-    let mut editor = rustyline::Editor::<()>::new()?;
-
-    if let Err(err) = editor.load_history(&sh.log_file) {
-        eprintln!("unable to read history file: {err}");
-    }
-
-    // - tx: transmitter (sender)
-    // - rx: receiver
     let (worker_tx, worker_rx) = mpsc::channel();
-    let (shell_tx, shell_rx) = mpsc::sync_channel(0);
+    let (shell_tx, mut shell_rx) = mpsc::sync_channel(0);
+
+    let mut state = State::create(&sh.log_file, worker_tx.clone())?;
 
     self::spawn_signal_handler(worker_tx.clone())?;
-    crate::worker::spawn_worker(worker_rx, shell_tx);
+    crate::worker::spawn_worker(worker_rx, shell_tx.clone());
 
     loop {
-        todo!();
+        if !state.process(sh, &mut shell_rx)? {
+            break;
+        }
     }
 
     Ok(())
 }
 
-/// Spawns the `signal_handler` threasd
+/// Spawns the `signal_handler` thread
 fn spawn_signal_handler(tx: mpsc::Sender<WorkerMsg>) -> Result<(), DynError> {
     let mut signals = signal_hook::iterator::Signals::new({
         use signal_hook::consts::*;
@@ -91,4 +87,83 @@ fn spawn_signal_handler(tx: mpsc::Sender<WorkerMsg>) -> Result<(), DynError> {
     });
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct State {
+    editor: rustyline::Editor<()>,
+    worker_tx: mpsc::Sender<WorkerMsg>,
+    exit_code: i32,
+    last_exit_code: i32,
+}
+
+impl State {
+    fn create(log_file: &str, worker_tx: mpsc::Sender<WorkerMsg>) -> rustyline::Result<Self> {
+        let mut editor = rustyline::Editor::<()>::new()?;
+
+        if let Err(err) = editor.load_history(log_file) {
+            eprintln!("unable to read history file: {err}");
+        }
+
+        Ok(Self {
+            editor,
+            worker_tx,
+            exit_code: 0,
+            last_exit_code: 0,
+        })
+    }
+
+    fn prompt(&self) -> String {
+        let face = if self.last_exit_code == 0 {
+            '\u{1F642}'
+        } else {
+            '\u{1F480}'
+        };
+
+        format!("ZeroSh {face} %>")
+    }
+
+    /// Returns `Ok(true)` if the shell can continue processing
+    fn process(
+        &mut self,
+        sh: &Shell,
+        shell_rx: &mut mpsc::Receiver<ShellMsg>,
+    ) -> Result<bool, DynError> {
+        let prompt = self.prompt();
+
+        // TODO: Allow multiline input (?)
+        use rustyline::error::ReadlineError;
+        let line = match self.editor.readline(&prompt) {
+            Ok(line) => line,
+            Err(ReadlineError::Interrupted) => {
+                todo!()
+            }
+            Err(ReadlineError::Eof) => {
+                todo!()
+            }
+            Err(err) => {
+                eprintln!("ZeroSh: read error\n{err}");
+                self.exit_code = 1;
+                return Ok(false);
+            }
+        };
+
+        let line = line.trim();
+        if line.is_empty() {
+            return Ok(true);
+        }
+        self.editor.add_history_entry(line);
+
+        self.worker_tx.send(WorkerMsg::Cmd {
+            cmd: line.to_string(),
+        })?;
+
+        match shell_rx.recv()? {
+            ShellMsg::Continue { code } => Ok(self.last_exit_code == code),
+            ShellMsg::Quit { code } => {
+                self.exit_code = code;
+                Ok(false)
+            }
+        }
+    }
 }
