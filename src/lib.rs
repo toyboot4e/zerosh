161e1375c788677c2d96dfe73e34eb1c57355599
toyboot4e <toyboot4e@gmail.com>
2022-12-17 16:42:43 +0900

    add `WorkerMsg` and `ShellMsg`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
 //! Zero Shell library
+//!
+//! # Threads
+//!
+//! - `main` reads user input and sends it to the `worker` thread.
+//! - `signal_handler` receieves signals and sends them to the `worker` thread.
+//! - `worker` is the core processor and manages processes.
 
 pub(crate) mod util;
+
+/// Message to the `worker` thread
+#[derive(Debug, Clone)]
+enum WorkerMsg {
+    /// Message from the `signal_handler` thread, i.e., a signal.
+    Signal { signal: i32 },
+
+    /// Message from the `main` thread, i.e., user input.
+    Cmd { cmd: String },
+}
+
+/// Message to the `main` thread
+#[derive(Debug, Clone)]
+enum ShellMsg {
+    /// Continue reading user input
+    Continue { code: i32 },
+
+    /// Quit the shell
+    Quit { code: i32 },
+}
 
